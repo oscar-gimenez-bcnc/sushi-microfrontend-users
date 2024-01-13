61f094b3f2c222d2302b42631f7382886d6f5701
OscarGimenez <o.gimenez.aldabas@gmail.com>
2024-01-13 10:32:20 +0100

    test: :white_check_mark: add download use case to cypress

--- cypress/e2e/users.cy.ts
+++ cypress/e2e/users.cy.ts
@@ -1,17 +1,31 @@
+const path = require('path');
+
 describe('UsersTable spec', () => {
-  it('should show a loader meanwhile data is fetched', () => {
-    cy.intercept('GET', 'https://jsonplaceholder.typicode.com/users', (req) => {
-      req.on('response', (res) => {
-        res.setDelay(10000);
-      });
-    });
+  beforeEach(() => {
     cy.visit('http://localhost:9001/');
-    cy.get('.loading.loading-spinner').should('be.visible');
   });
 
   it('should show 10 users when data is fetched', () => {
-    cy.visit('http://localhost:9001/');
     cy.get('.loading.loading-spinner').should('not.exist');
     cy.get('tbody tr').should('have.length', 10);
+  });
+
+  it('should download a .json file', () => {
+    cy.get('.loading.loading-spinner').should('not.exist');
+    cy.get('tbody tr').should('have.length', 10);
+
+    cy.get('tbody tr').first().find('[aria-label="Download button"]').click();
+    const downloadsFolder = Cypress.config('downloadsFolder');
+    cy.readFile(path.join(downloadsFolder, 'user_1.json')).should('exist');
+  });
+
+  it('should download a .csv file', () => {
+    cy.get('.loading.loading-spinner').should('not.exist');
+    cy.get('tbody tr').should('have.length', 10);
+
+    cy.get('[aria-label="Change download method"]').click();
+    cy.get('tbody tr').first().find('[aria-label="Download button"]').click();
+    const downloadsFolder = Cypress.config('downloadsFolder');
+    cy.readFile(path.join(downloadsFolder, 'user_1.csv')).should('exist');
   });
 });
