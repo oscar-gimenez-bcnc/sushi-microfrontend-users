d9c7eef03e7087575ba91a18d0973a8c4a869f1b
OscarGimenez <o.gimenez.aldabas@gmail.com>
2024-01-13 08:39:31 +0100

    test: :white_check_mark: add download use case tests

---
+++ src/application/downloadUser/downloadUser.test.tsx
@@ -0,0 +1,38 @@
+import { mockedUsers } from '../../../__mocks__/users';
+import { IUser } from '../../domain/models/IUser';
+import { createCsvUserDownloader } from '../../infrastructure/dataDownload/CsvUserDownloader';
+import { createJsonUserDownloader } from '../../infrastructure/dataDownload/JsonUserDownloader';
+
+beforeAll(() => {
+  global.fetch = jest.fn();
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
+it('should create a download link and generate a blob URL when downloading user data as CSV', async () => {
+  const user: IUser = mockedUsers[0];
+  const csvExporter = createCsvUserDownloader();
+
+  jest.spyOn(document, 'createElement');
+  global.URL.createObjectURL = jest.fn(() => 'mocked blob');
+
+  await csvExporter.download(user);
+
+  expect(document.createElement).toHaveBeenCalledWith('a');
+  expect(global.URL.createObjectURL).toHaveBeenCalled();
+});
+
+it('should create a download link and generate a blob URL when downloading user data as JSON', async () => {
+  const user: IUser = mockedUsers[0];
+  const csvExporter = createJsonUserDownloader();
+
+  jest.spyOn(document, 'createElement');
+  global.URL.createObjectURL = jest.fn(() => 'mocked blob');
+
+  await csvExporter.download(user);
+
+  expect(document.createElement).toHaveBeenCalledWith('a');
+  expect(global.URL.createObjectURL).toHaveBeenCalled();
+});
