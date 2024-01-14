086bf85264f73ba0191fcdf4905bfcfdd4f64a85
OscarGimenez <o.gimenez.aldabas@gmail.com>
2024-01-14 20:05:29 +0100

    feat: :lipstick: show different header if is part of a shell app

--- src/ui/components/MainLayout/components/Header/index.tsx
+++ src/ui/components/MainLayout/components/Header/index.tsx
@@ -5,13 +5,19 @@ import useHeader from './useHeader';
 const Header: React.FC = () => {
   const {
     actions: { handleChangeDataSource, handleChangeDownloadMethod },
-    states: { dataSource, downloadMethod }
+    states: { dataSource, downloadMethod, isCacheEnabled }
   } = useHeader();
 
   return (
     <div className="flex items-center justify-between gap-2">
       <div className="flex flex-row items-baseline gap-2">
-        <h1 className="text-xl font-bold">Sushi Photos</h1> <span className="text-sm text-gray-600">Users</span>
+        {isCacheEnabled === true ? (
+          <span className="text-gray-600">Users Microfrontend</span>
+        ) : (
+          <>
+            <h1 className="text-xl font-bold">Sushi Photos</h1> <span className="text-sm text-gray-600">Users</span>
+          </>
+        )}
       </div>
       <div className="flex items-center justify-end gap-2">
         <h5 className="flex flex-row items-center gap-2 text-sm">
