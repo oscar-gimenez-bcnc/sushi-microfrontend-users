8fa332de0cf2d15f562139623c0238c99a8bcadd
OscarGimenez <o.gimenez.aldabas@gmail.com>
2024-01-15 19:00:05 +0100

    fix: :bug: remove actions in responsive

--- src/ui/components/MainLayout/components/Header/index.tsx
+++ src/ui/components/MainLayout/components/Header/index.tsx
@@ -19,7 +19,7 @@ const Header: React.FC = () => {
           </>
         )}
       </div>
-      <div className="flex items-center justify-end gap-2">
+      <div className="hidden items-center justify-end gap-2 md:flex">
         <h5 className="flex flex-row items-center gap-2 text-sm">
           <span className="badge badge-ghost badge-xs gap-2 p-2">
             {downloadMethod}
